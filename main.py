d13e9a20cd8ebfe2012f7278e12b291c4e846b14
Maha244 <73404933+Maha244@users.noreply.github.com>
2021-09-12 17:48:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# My code
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -26,9 +26,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-# i used two ways with the '' problem اذا ممكن نستخدم
-# \" or \' to egnore the '' line 31 and 33 is example of that
 
 step1 = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"?\n ")
 if step1 == "left" or step1 == "Left":
